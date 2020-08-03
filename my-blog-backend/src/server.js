6075879923731682bb8e9fd5312bd33cf53a3a86
Sumithpd <https://github.com/sumithpdd/flutter_complete_guide.git>
2020-08-03 14:34:47 +0200

    Using Mongodb as backend

--- my-blog-backend/src/server.js
+++ my-blog-backend/src/server.js
@@ -1,20 +1,20 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-
-const articlesInfo = {
-    'learn-react': {
-        upvotes: 0,
-        comments: []
-    },
-    'learn-node': {
-        upvotes: 0,
-        comments: []
-    },
-    'my-thoughts-on-resumes': {
-        upvotes: 0,
-        comments: []
-    },
-}
+import { MongoClient } from 'mongodb';
+// const articlesInfo = {
+//     'learn-react': {
+//         upvotes: 0,
+//         comments: []
+//     },
+//     'learn-node': {
+//         upvotes: 0,
+//         comments: []
+//     },
+//     'my-thoughts-on-resumes': {
+//         upvotes: 0,
+//         comments: []
+//     },
+// }
 
 const app = express();
 app.use(bodyParser.json());
@@ -22,18 +22,64 @@ app.use(bodyParser.json());
 // app.get("/hello", (req, res) => res.send('Hello!'));
 
 // app.get("/hello/:name", (req, res) => res.send(`Hello ${req.params.name}!`));
+const withDB = async(operations, res) => {
+    try {
 
-app.post("/api/articles/:name/upvote", (req, res) => {
-    const articleName = req.params.name;
+        const client = await MongoClient.connect('mongodb://localhost:27017', { useNewUrlParser: true });
+        const db = client.db('my-blog');
 
-    articlesInfo[articleName].upvotes += 1;
-    res.status(200).send(`${articleName} now has ${articlesInfo[articleName].upvotes} upvotes`)
+        await operations(db);
+        client.close();
+    } catch (error) {
+        res.status(500).json({ message: 'Error connecting to db', error });
+    }
+}
+app.get("/api/articles/:name", async(req, res) => {
+    withDB(async(db) => {
+        const articleName = req.params.name;
+        const articleInfo = await db.collection('articles').findOne({ name: articleName });
+        res.status(200).json(articleInfo);
+    }, res)
+});
+
+app.post("/api/articles/:name/upvote", async(req, res) => {
+    withDB(async(db) => {
+        const articleName = req.params.name;
+
+        const articleInfo = await db.collection('articles').findOne({ name: articleName });
+        await db.collection('articles').updateOne({
+            name: articleName
+        }, {
+            '$set': {
+                upvotes: articleInfo.upvotes + 1,
+            },
+        });
+
+        const updatedArticleInfo = await db.collection('articles').findOne({ name: articleName });
+
+        res.status(200).json(updatedArticleInfo);
+    }, res);
+
 });
 app.post("/api/articles/:name/add-comment", (req, res) => {
     const articleName = req.params.name;
     const { username, text } = req.body;
 
-    articlesInfo[articleName].comments.push({ username, text });
-    res.status(200).send(articlesInfo[articleName]);
+    // articlesInfo[articleName].comments.push({ username, text });
+    // res.status(200).send(articlesInfo[articleName]);
+    withDB(async(db) => {
+        const articleInfo = await db.collection('articles').findOne({ name: articleName });
+        await db.collection('articles').updateOne({
+            name: articleName
+        }, {
+            '$set': {
+                comments: articleInfo.comments.concat({ username, text })
+            },
+        });
+
+        const updatedArticleInfo = await db.collection('articles').findOne({ name: articleName });
+
+        res.status(200).json(updatedArticleInfo);
+    }, res)
 });
 app.listen(8000, () => console.log('Listening on part 8000'));
