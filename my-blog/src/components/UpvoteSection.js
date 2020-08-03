35de8b8c69128824b55d831aab0c57e0e43f9e1a
Sumithpd <https://github.com/sumithpdd/flutter_complete_guide.git>
2020-08-03 19:22:32 +0200

    Upvote and comment finished

---
+++ my-blog/src/components/UpvoteSection.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const UpvoteSection = ({ articleName, upvotes, setArticleInfo }) => {
+    const upvoteArticle = async () => {
+        const result = await fetch(`/api/articles/${articleName}/upvote`,
+            {
+                method: 'post',
+            });
+        const body = await result.json();
+        setArticleInfo(body);
+    }
+    return (
+        <div id="upvote-section">
+            <button onClick={() => upvoteArticle()}>Add Upvote</button>
+            <p> This post has been upvoted {upvotes} times</p>
+        </div>
+    );
+}
+
+export default UpvoteSection;
--- my-blog/src/pages/ArticlePage.js
+++ my-blog/src/pages/ArticlePage.js
@@ -1,23 +1,42 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import articleContent from './article-content';
 import ArticlesList from '../components/ArticlesList';
 import NotFoundPage from './NotFoundPage';
+import CommentsList from '../components/CommentsList';
+import UpvoteSection from '../components/UpvoteSection';
+import AddCommentForm from '../components/AddCommentForm';
 
 const ArticlePage = ({ match }) => {
     const name = match.params.name;
     const article = articleContent.find(article => article.name === name);
     // if (!article) return <h1>Article doesnot exist!</h1>
 
-    if (!article) return <NotFoundPage/>
+    const [articleInfo, setArticleInfo] = useState({ upvotes: 0, comments: [] });
 
-    const otherArticles = articleContent.filter(article => article.name != name);
+    useEffect(() => {
+
+        const fetchData = async () => {
+            const result = await fetch(`/api/articles/${name}`);
+            const body = await result.json();
+            setArticleInfo(body);
+        }
+        fetchData();
+    }, [name])
+
+    if (!article) return <NotFoundPage />
+
+    const otherArticles = articleContent.filter(article => article.name !== name);
 
     return (
         <>
             <h1>{article.title}</h1>
+            <UpvoteSection articleName={name} upvotes={articleInfo.upvotes} setArticleInfo={setArticleInfo}/>
+            
             {article.content.map((paragraph, key) => (
                 <p key={key}>{paragraph}</p>
             ))}
+            <CommentsList comments={articleInfo.comments}/>
+            <AddCommentForm articleName={name} setArticleInfo={setArticleInfo}/>
             <h3>Other Articles:</h3>
             <ArticlesList articles={otherArticles} />
         </>
